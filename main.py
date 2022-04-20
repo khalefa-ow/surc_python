a67069f99fa7d6e6d02a07d729ef87a54abf6f1e
User <khalefaow@gmail.com>
2022-04-20 04:01:01 -0400

    start gen

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+from doctest import Example
 import parse as p
 import tree as t
+import example as e
 
 from anytree import NodeMixin, RenderTree
+from pglast.visitors import Visitor
+from anytree import NodeMixin, RenderTree, PreOrderIter
 
 
-sql="select * from f a, h where f.bar = 'baz' and f.x=h.y and f.m>3 group by a having count(*)>100 order by x;"
+sql="select * from f a, h where a.bar = 'baz' and a.x=h.y and f.m>3 group by a having count(*)>100 order by x;"
+#sql="select * from f a, h where a.bar = 'baz' "
 #" select * from (select * from b)a;"
 v=p.parse(sql)
 
 for s in v:
     r=t.buildtree(s)
     for pre, fill, node in RenderTree(r):
-     print("%s%s %s" % (pre, node.id, node.type)) 
+     print("%s%s %s" % (pre, node.id, node.type))
+    for x in PreOrderIter(r):
+        print (x.emitcode(1,e.context))
--- tree.py
+++ tree.py
@@ -1,9 +1,10 @@
 from cmath import exp
-import re
+
 from sqlite3 import paramstyle
-from anytree import NodeMixin, RenderTree
+from anytree import NodeMixin, RenderTree, PreOrderIter
 
 from enum import Enum
+
 
 
 class NodeType(Enum): 
@@ -18,31 +19,55 @@ class NodeType(Enum):
     CROSSPRODUCT=9
     RESULT=0
     NONE=10
+    
+    def __str__(self):
+        return str(self.name)+"= "+str(self.value)
+ 
 
 class Op:
-    def __init__(self, nd):
-        self.type= nd
     def __init__(self):
         self.type= NodeType.NONE  
-    def emitcode(self):
-        return ""
-# and a function to emit code    
-
-
-
+   
 
 class Operator(Op, NodeMixin):  # Add Node feature
     nodeid =0
-    def __init__(self, type, params, parent=None, children=None):
+    def __init__(self, dtype, params, parent=None):
         super(Operator, self).__init__()
         self.id = Operator.nodeid
         Operator.nodeid += 1
-        self.type=type
+        self.optype=dtype
         self.params=params
         self.parent = parent
-       
-        if children:
-             self.children = children
+        self.child=[]
+
+    def __str__(self):
+        return "id "+ str(self.id) + "optype "+ str(self.optype) + " "+str(self.params) +  " "+str(self.child)   
+
+    def emitcode(self, n, context):
+        func="operator_"+ str(self.id)
+        indent='\t'*n
+        print(indent +"#" +str(self.optype)+"\n")
+        print(indent+"def "+func+':'+'\n')
+        
+        if self.params != None:
+            for x,v in self.params.items():
+                print(indent+ " "+ str(x) + " "+ str(v)+ "\n")
+        if self.optype==NodeType.SCAN :
+            # assume a scan csv file
+            lines=[]
+            self.params["filename"]=context.Relations[self.params["relname"]]
+            lines.append("with open('{filename}', newline='') as csv_file:")
+
+            lines.append("    csv_reader = csv.DictReader(csv_file, delimiter=',')")
+            lines.append("    for row in csv_reader:")
+            lines.append("        yield row")
+	
+            #lines.append("\t\tprint(f'Column names are \{\", \".join(row)}\')")
+            
+            for line in lines:
+                print(indent+ line.format(**self.params)+"")
+
+            #assume a scan json file 
 
 def get_rel_cond(relname, context):
     conds=[]
@@ -56,9 +81,9 @@ def get_rel_cond(relname, context):
                     rexpr=arg['rexpr']
                     lrelname=None
                     rrelname=None
-                    if len(lexpr['fields'])>1 : 
+                    if lexpr['@']=='ColumnRef' and lexpr['fields'] !=None and len(lexpr['fields'])>1 : 
                         lrelname=(lexpr['fields'])[0]['val']
-                    if len(rexpr['fields'])>1 : 
+                    if  rexpr['@']=='ColumnRef' and rexpr['fields'] !=None and len(rexpr['fields'])>1 : 
                         rrelname=(rexpr['fields'])[0]['val']
                     lcond= lexpr['@']=='ColumnRef' and rexpr['@']=='Const' and lrelname==relname
                     rcond=rexpr['@']=='ColumnRef' and lexpr['@']=='Const' and rrelname==relname
@@ -75,10 +100,9 @@ def get_rel_cond(relname, context):
     return conds
 
 def get_join_cond(l,r,context):
-    #l can be a list
     conds=[]
     if context != None:
-            w=context.whereClause
+            w=context.whereClause()
             if w != None:
                 args=w['args']
                 conds=[]
@@ -87,14 +111,15 @@ def get_join_cond(l,r,context):
                     rexpr=arg['rexpr']
                     lrelname=None
                     rrelname=None
-                    if len(lexpr['fields'])>1 : 
+                    if lexpr['@']=='ColumnRef' and lexpr['fields'] !=None and  len(lexpr['fields'])>1 : 
                         lrelname=(lexpr['fields'])[0]['val']
-                    if len(rexpr['fields'])>1 : 
+                    if rexpr['@']=='ColumnRef' and rexpr['fields'] !=None and  len(rexpr['fields'])>1 : 
                         rrelname=(rexpr['fields'])[0]['val']
                     cond= lexpr['@']=='ColumnRef' and rexpr['@']=='ColumnRef' and ((lrelname in l and rrelname in r) or ((lrelname in r and rrelname in l))) 
                     
                     if cond:
                         conds.append(arg)
+                        
 
     return conds
 
@@ -102,7 +127,9 @@ def get_relname(rel):
     relname=""
     if type(rel).__name__=='RangeVar':
         relname=rel.relname
-    return relname    
+        if rel.alias != None:
+            relname=rel.alias.aliasname
+    return relname 
              
 def process_scan(rel, context):
     relname=""
@@ -116,7 +143,9 @@ def process_scan(rel, context):
     #find any  where 
         conds=get_rel_cond(relname,context)
         params['conds']=conds
-        filter=Operator(NodeType.FILTER,params,scan)
+        filter=Operator(NodeType.FILTER,params)
+        filter.child.append(scan)
+        scan.parent=filter
 
     if filter==None:
         return scan
@@ -126,29 +155,41 @@ def process_scan(rel, context):
 def process_from(fromClause, context):
     #to do:
     # sort relation by the condition
+    l=[]
+    r=[]
     length=len(fromClause)
     if length==1:
         return process_scan(fromClause[0],context)
     elif length>=2:
         prev=process_scan(fromClause[0],context)
         rel1=get_relname(fromClause[0])
-        l=[].append(rel1)
+        l.append(rel1)
         
         for i in range(1,length):
             scan2=process_scan(fromClause[i],context)
             rel2=get_relname(fromClause[i])
-            r=[].append(rel2)
+            r.append(rel2)
             conds=get_join_cond(l,r,context)
             if len(conds)>0:
-                params=[]
+                params=dict()
                 params['conds']=conds
-                join=Operator(NodeType.JOIN,params,prev,scan2)
+                join=Operator(NodeType.JOIN,params)
+                join.child.append( prev)
+                join.child.append( scan2)
+                prev.parent=join
+                scan2.parent=join
                 prev=join
                 l.append(rel2)
             else:
-                crossproduct=Operator(NodeType.CROSSPRODUCT,None,prev,scan2)
+                crossproduct=Operator(NodeType.CROSSPRODUCT,None)
+                prev.parent=crossproduct
+                scan2.parent=crossproduct
+                crossproduct.child.append(prev)
+                crossproduct.child.append( scan2)
+
                 prev=crossproduct
     return prev       
+
 
 
 
@@ -158,8 +199,9 @@ def buildtree(s):
     #convert from
     fclause=s.fromClause
     f=process_from(fclause,s)
-    result=Operator(NodeType.RESULT)
+    result=Operator(NodeType.RESULT,None)
     f.parent=result
+    result.child.append(f)
     #whereClause
     
     #joincondition
